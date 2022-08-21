c96b805e18e8ac4d99b0cc01e512a45232837e46
Koruto <flyingshadow03@gmail.com>
2022-08-21 21:42:11 +0530

    Win Conditions for Horizontal and Vertical Done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 var tile = document.getElementsByClassName('tile');
 var activePlayer = 0;
+var checkTile;
+var count = 0;
 
 document.querySelector('.play').addEventListener('click', function () {
   /* Change Screen to Game Screen */ /* !  */ document
@@ -22,21 +24,60 @@ Array.prototype.forEach.call(tile, function (element) {
   // element.addEventListener('mouseleave', function () {
   //   element.style.backgroundColor = '#423f3ea2';
   // });
-
+  console.log(element);
+  console.log(1);
   /* Clicking Tile */ element.addEventListener('click', function () {
+    if (activePlayer) checkTile = 'blue';
+    else checkTile = 'red';
+
     if (!element.classList.contains('clicked')) {
       if (activePlayer) {
         document.getElementById(element.id).innerHTML +=
           '<div class="center blueTile"></div>';
-        activePlayer = 0;
+        element.classList.add('blue');
       } else {
         document.getElementById(element.id).innerHTML +=
           '<div class="center redTile"></div>';
-        activePlayer = 1;
+        element.classList.add('red');
       }
       element.classList.add('clicked');
     } else {
       console.log('Tile Already Exists!'); /* TODO Better Error */
     }
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 7; j++) {
+        let tileName = 'tile-' + i + j;
+        let tN = document.getElementById(tileName);
+
+        if (tN.classList.contains(checkTile)) {
+          count += 1;
+        } else {
+          count = 0;
+        }
+
+        if (count == 4) window.alert(checkTile + ' Wins!');
+      }
+      count = 0;
+    }
+
+    for (let j = 0; j < 7; j++) {
+      for (let i = 0; i < 6; i++) {
+        let tileName = 'tile-' + i + j;
+        let tN = document.getElementById(tileName);
+
+        if (tN.classList.contains(checkTile)) {
+          count += 1;
+        } else {
+          count = 0;
+        }
+
+        if (count == 4) window.alert(checkTile + ' Wins!');
+      }
+      count = 0;
+    }
+
+    if (activePlayer) activePlayer = 0;
+    else activePlayer = 1;
   });
 });
